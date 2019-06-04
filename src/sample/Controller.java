53a576c03bdde4a13c4b6874fc957c1863e8e13e
tamazyanarsen <tamazyanarsen@mail.ru>
2019-06-04 09:04:08 +0300

    подключил все нужные библиотеки!!! интерфейс через sceneBuilder

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,98 @@
 package sample;
 
+
+import com.supermax.javapos.service.SuperScan;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import jpos.BaseControl;
+import jpos.JposException;
+import jpos.config.simple.SimpleEntry;
+import jpos.config.simple.xml.SimpleXmlRegPopulator;
+import jpos.events.*;
+import jpos.services.EventCallbacks;
+
 public class Controller {
+    @FXML
+    private Button btnOpen, btnEnabled, btnClaim;
+
+    private SuperScan superScan = new SuperScan();
+
+
+    @FXML
+    public void initialize() {
+        SimpleEntry simpleEntry = new SimpleEntry(new SimpleXmlRegPopulator());
+        superScan.setJposEntry(simpleEntry);
+
+        GridPane gridPane = new GridPane();
+        btnOpen.setOnMouseClicked(event -> {
+            System.out.println("openBtn clicked. Start open function");
+            try {
+                superScan.open("DL-Portal-Scanner", new EventCallbacks() {
+                    @Override
+                    public void fireDataEvent(DataEvent dataEvent) {
+                        System.out.println("data event: " + dataEvent.toString());
+                    }
+
+                    @Override
+                    public void fireDirectIOEvent(DirectIOEvent directIOEvent) {
+
+                    }
+
+                    @Override
+                    public void fireErrorEvent(ErrorEvent errorEvent) {
+
+                    }
+
+                    @Override
+                    public void fireOutputCompleteEvent(OutputCompleteEvent outputCompleteEvent) {
+
+                    }
+
+                    @Override
+                    public void fireStatusUpdateEvent(StatusUpdateEvent statusUpdateEvent) {
+
+                    }
+
+                    @Override
+                    public BaseControl getEventSource() {
+                        return null;
+                    }
+                });
+                btnOpen.setText("Закрыть");
+            } catch (JposException e) {
+                e.printStackTrace();
+            }
+        });
+
+        btnClaim.setOnMouseClicked(event -> {
+            try {
+                superScan.claim(1000);
+                btnClaim.setText("Освободить");
+            } catch (JposException e) {
+                e.printStackTrace();
+                btnClaim.setText("Занять");
+            }
+        });
+
+        btnEnabled.setOnMouseClicked(event -> {
+            try {
+                boolean value = superScan.getDeviceEnabled();
+                if (value) btnEnabled.setText("Отключить");
+                else btnEnabled.setText("Включить");
+                superScan.setDeviceEnabled(!value);
+            } catch (JposException e) {
+               e.printStackTrace();
+            }
+        });
+
+//        btnOpen.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+//            @Override
+//            public void handle(MouseEvent mouseEvent) {
+//                btnOpen.setText("Закрыть");
+//            }
+//        });
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,70 +2,29 @@ package sample;
 
 import com.supermax.javapos.service.SuperScan;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import jpos.BaseControl;
+import jpos.JposException;
 import jpos.config.simple.SimpleEntry;
 import jpos.config.simple.xml.SimpleXmlRegPopulator;
 import jpos.events.*;
 import jpos.services.EventCallbacks;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setScene(new Scene(root, 300, 275));
+//        GridPane root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 //        primaryStage.setScene(new Scene(root, 300, 275));
-        GridPane gridPane = new GridPane();
-        Button open = new Button("Открыть");
-        open.setOnMouseClicked(event -> {
-            System.out.println("get event" + event);
-        });
 
-        SuperScan superScan = new SuperScan();
-        SimpleEntry simpleEntry = new SimpleEntry(new SimpleXmlRegPopulator());
-        superScan.setJposEntry(simpleEntry);
-        try {
-            superScan.open("DL-Portal-Scanner", new EventCallbacks() {
-                @Override
-                public void fireDataEvent(DataEvent dataEvent) {
 
-                }
-
-                @Override
-                public void fireDirectIOEvent(DirectIOEvent directIOEvent) {
-
-                }
-
-                @Override
-                public void fireErrorEvent(ErrorEvent errorEvent) {
-
-                }
-
-                @Override
-                public void fireOutputCompleteEvent(OutputCompleteEvent outputCompleteEvent) {
-
-                }
-
-                @Override
-                public void fireStatusUpdateEvent(StatusUpdateEvent statusUpdateEvent) {
-
-                }
-
-                @Override
-                public BaseControl getEventSource() {
-                    return null;
-                }
-            });
-        } catch (Exception e) {
-            throw e;
-//            e.printStackTrace();
-        }
-
-        gridPane.getChildren().add(open);
-        primaryStage.setScene(new Scene(gridPane, 300, 275));
+//        primaryStage.setScene(new Scene(gridPane, 300, 275));
         primaryStage.setTitle("Hello World");
         primaryStage.show();
     }
